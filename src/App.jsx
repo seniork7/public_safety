5c1165aadc732a37de420707d8c4774ada019f46
Kevon Senior <98934481+seniork7@users.noreply.github.com>
2025-11-19 17:34:35 -0500

    Fix vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header.jsx'
 import Home from './components/pages/Home.jsx'
 import About from './components/pages/About.jsx'
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import babel from "vite-plugin-babel";
-import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(), 
-    tailwindcss(), 
-    reactRouter(),
-    babel()
-  ],
+  plugins: [react(), tailwindcss()],
   base: './',
 })
